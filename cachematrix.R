5b1e0c7f67843dc2aebb4b73056d48da02180456
Sheepwalrus <jdphd7@gmail.com>
2020-06-28 13:22:42 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# makeCacheMatrix takes in a matrix X and converts it into a matrix Object
+# capable of storing functions, the original matrix, and its inverse
+
+# cacheSolve takes in the matrix object created by makeCacheMatrix
+# if the inverse has been already calculated, it uses that. Otherwise, it calc's the inverse
+
+
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# makeCacheMatrix takes in a matrix X and converts it into a matrix Object
 
+# This object has 4 functions and 2 floating variables. 
+# Functions: set, get, setInverse, and getInverse
+# x, the original Matrix passed in, and inverseMatrix, the inverse of X if calculated
+
+# Set replaces the current matrix stored in the object with a new one passed in. 
+# Get retrieves the current matrix stored in the object
+# SetInverse defines the Inverse Matrix in the object as the matrix passed in.
+# GetInverse retrieves the Inverse Matrix currently stored in the object.
+
+# list allows the commands to be callable using the $ operator
+makeCacheMatrix <- function(x = matrix())
+{
+  inverseMatrix <- NULL
+  
+  set <- function(y)
+  {
+    x <<- y
+    inverseMatrix <<- NULL
+  }
+  
+  get <- function()
+  {
+    x
+  }
+  
+  setInverse <- function(newInverse)
+  {
+    inverseMatrix <<- newInverse
+  }
+  
+  getInverse <- function()
+  {
+    inverseMatrix
+  }
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#cacheSolve takes in a matrixObject and either returns the cached Inverse Matrix stored within the object if available
+#or calculates the InverseObject and stores it back in the object. 
+
+cacheSolve <- function(x, ...) 
+{
+  inverseMatrix <- x$getInverse
+  if(!is.null(inverseMatrix))
+  {
+    message("getting cached data")
+    return(inverseMatrix)
+  }
+  matrix = x$get()
+  inverseMatrix = solve(matrix,...)
+  x$setInverse(inverseMatrix)
+  inverseMatrix
 }
